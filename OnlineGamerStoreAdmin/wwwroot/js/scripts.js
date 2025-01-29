662dd9d9f5e2802e0a8673d450fa14ebf8555704
Michael Yara G <myarag17@gmail.com>
2025-01-29 15:08:56 -0500

    Cambios Front - Bootstrap

--- OnlineGamerStoreAdmin/wwwroot/js/scripts.js
+++ OnlineGamerStoreAdmin/wwwroot/js/scripts.js
@@ -1,7 +1,11 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+/*!
+    * Start Bootstrap - SB Admin v7.0.7 (https://startbootstrap.com/template/sb-admin)
+    * Copyright 2013-2023 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
+    */
+    // 
+// Scripts
+// 
 
 window.addEventListener('DOMContentLoaded', event => {
 
